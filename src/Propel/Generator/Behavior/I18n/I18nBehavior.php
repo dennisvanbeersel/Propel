00a52609fcd0a34778104e5b5acf734d928be649
Dennis Van Beersel <me@denvb.eu>
2026-05-06 12:46:19 +0200

    fix(i18n): remove dead Validate\\ValidateBehavior references
    The validate behavior class was removed in A.22 dead-code cleanup
    but I18nBehavior::moveI18nColumns() still carried code paths that
    instantiated and called methods on it. Drop the entire dead block:
    
      - the use import for the missing class
      - the inner if (\$table->hasBehavior('validate')) ... rule-moving
        branch that called \$validateBehavior->getParametersFromColumnName()
        et al.
      - the trailing \"current table must have at least 1 validation rule\"
        block that called new ValidateBehavior() and \$validate->addRuleOnPk()
    
    Also tighten getI18nColumnNamesFromConfig(): the previous implementation
    unset() entries inside a foreach, producing a sparse array that violated
    its declared list<string> return type. Rewrite as a clean loop over
    explode() that only appends non-empty trimmed names.
    
    10 phpstan errors gone at the source.

--- src/Propel/Generator/Behavior/I18n/I18nBehavior.php
+++ src/Propel/Generator/Behavior/I18n/I18nBehavior.php
@@ -10,7 +10,6 @@ declare(strict_types=1);
 
 namespace Propel\Generator\Behavior\I18n;
 
-use Propel\Generator\Behavior\Validate\ValidateBehavior;
 use Propel\Generator\Builder\Om\AbstractOMBuilder;
 use Propel\Generator\Exception\EngineException;
 use Propel\Generator\Model\Behavior;
@@ -322,7 +321,6 @@ class I18nBehavior extends Behavior
         $table = $this->getTable();
         $i18nTable = $this->i18nTable;
 
-        $i18nValidateParams = [];
         foreach ($this->getI18nColumnNamesFromConfig() as $columnName) {
             if (!$i18nTable->hasColumn($columnName)) {
                 if (!$table->hasColumn($columnName)) {
@@ -331,34 +329,12 @@ class I18nBehavior extends Behavior
 
                 $column = $table->getColumn($columnName);
                 $i18nTable->addColumn(clone $column);
-
-                // validate behavior: move rules associated to the column
-                if ($table->hasBehavior('validate')) {
-                    /** @var \Propel\Generator\Behavior\Validate\ValidateBehavior $validateBehavior */
-                    $validateBehavior = $table->getBehavior('validate');
-                    $params = $validateBehavior->getParametersFromColumnName($columnName);
-                    $i18nValidateParams = array_merge($i18nValidateParams, $params);
-                    $validateBehavior->removeParametersFromColumnName($columnName);
-                }
                 // FIXME: also move FKs, and indices on this column
             }
 
             if ($table->hasColumn($columnName)) {
                 $table->removeColumn($columnName);
             }
-        }
-
-        // validate behavior
-        if (count($i18nValidateParams) > 0) {
-            $i18nVbehavior = new ValidateBehavior();
-            $i18nVbehavior->setName('validate');
-            $i18nVbehavior->setParameters($i18nValidateParams);
-            $i18nTable->addBehavior($i18nVbehavior);
-
-            // current table must have almost 1 validation rule
-            /** @var \Propel\Generator\Behavior\Validate\ValidateBehavior $validate */
-            $validate = $table->getBehavior('validate');
-            $validate->addRuleOnPk();
         }
     }
 
@@ -391,15 +367,11 @@ class I18nBehavior extends Behavior
      */
     protected function getI18nColumnNamesFromConfig(): array
     {
-        $columnNames = explode(',', $this->getParameter('i18n_columns'));
-
-        foreach ($columnNames as $key => $columnName) {
+        $columnNames = [];
+        foreach (explode(',', $this->getParameter('i18n_columns')) as $columnName) {
             $columnName = trim($columnName);
-
-            if ($columnName) {
-                $columnNames[$key] = $columnName;
-            } else {
-                unset($columnNames[$key]);
+            if ($columnName !== '') {
+                $columnNames[] = $columnName;
             }
         }
 
